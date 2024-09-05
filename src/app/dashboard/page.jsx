c381f97c252846cf7bf8e8cdedf8af3a12f84a0f
Angella-Mulikatete <mulikateteangella@gmail.com>
2024-09-05 09:51:19 +0300

    worked on ui

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,48 +1,12 @@
-"use client";
+import React from 'react'
+import Posts from '@/components/Posts'
 
-import { useState } from "react";
-import AuxBar from "@/components/AuxBar";
-import CreatePost from "@/components/CreatePost";
-import Header from "@/components/Header";
-import PostCard from "@/components/PostCard";
-import PostModal from "../components/PostModal";
-
-export default function Page() {
-    const [open, setOpen] = useState<boolean>(false);
-    return(
-        <div className="w-full h-screen">
-            {open &&(
-                <PostModal
-                    open={open}
-                    setOpen={setOpen}
-                />
-            )}
-            <Header accessGranted={true} />
-
-            <div
-                className="px-8 flex flex-row justify-between"
-            >
-                <div
-                    className="w-3/12"
-                >
-                    <CreatePost 
-                        open={open}
-                        setOpen={setOpen}
-                    />
-                </div>
-
-                <div
-                    className="w-5/12"
-                >
-                    <PostCard />
-                </div>
-
-                <div
-                    className="w-3/12"
-                >
-                    <AuxBar />
-                </div>
-            </div>
-        </div>
-    )
+const Page = () => {
+  return (
+    <div>
+       <Posts />
+    </div>
+  )
 }
+
+export default Page
--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -36,7 +36,7 @@ const CreatePost = ({
             <p
                 className="font-bold text-2xl mb-4"
             >
-                What's happening around you today?
+                What is happening around you today?
             </p>
 
             <button
