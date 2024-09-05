aea6bf191335d6934b558c81ec0de2de0807435e
Angella-Mulikatete <mulikateteangella@gmail.com>
2024-09-05 09:06:10 +0300

    added react-icons

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -15,39 +15,39 @@ const Landing = () => {
 
   // let contract = web3.eth.Contract(ContractABI,contractAddress);
  
-  async function requestAccount(){
-    console.log("Requesting account...");
-    if(window.ethereum){
-      try{
-        console.log("metamask detected");
-        const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
-        // console.log("metamsk accounts",accounts);
-         setWalletAddress(accounts[0]);
+  // async function requestAccount(){
+  //   console.log("Requesting account...");
+  //   if(window.ethereum){
+  //     try{
+  //       console.log("metamask detected");
+  //       const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+  //       // console.log("metamsk accounts",accounts);
+  //        setWalletAddress(accounts[0]);
 
-        // Save the wallet address in local storage
-        localStorage.setItem('walletAddress', accounts[0]);
+  //       // Save the wallet address in local storage
+  //       localStorage.setItem('walletAddress', accounts[0]);
 
-        // Redirect to profile page if wallet is connected
-        if (accounts.length > 0) {
-          router.push("/profile");
-        } else {
-          console.log("No accounts found.");
-        }
+  //       // Redirect to profile page if wallet is connected
+  //       if (accounts.length > 0) {
+  //         router.push("/profile");
+  //       } else {
+  //         console.log("No accounts found.");
+  //       }
 
-      }catch(err){
-         if (err.code === 4001) {
-           // EIP-1193 userRejectedRequest error.
-            console.log("Please connect to MetaMask.");
-          } else {
-                console.error(err);
-            }
-      }
+  //     }catch(err){
+  //        if (err.code === 4001) {
+  //          // EIP-1193 userRejectedRequest error.
+  //           console.log("Please connect to MetaMask.");
+  //         } else {
+  //               console.error(err);
+  //           }
+  //     }
       
-    }else{
-      console.log("metamask not detected");
-      alert("Please install metamask and connect to a network");
-    }
-  }
+  //   }else{
+  //     console.log("metamask not detected");
+  //     alert("Please install metamask and connect to a network");
+  //   }
+  // }
 
   return (
       <div
@@ -77,11 +77,10 @@ const Landing = () => {
                     <p
                         className='font-semibold text-sm text-white w-10/12 mb-2'
                     >
-                    Scan QR code and Login to Acolyte or...
-                    Login to Votechat
+                    Scan QR code and Login to Acolyte
                     </p>
 
-                    <button onClick={requestAccount}
+                    {/* <button onClick={requestAccount}
                         className='flex flex-row justify-between items-center h-[30px] bg-slate-500/50 py-2 px-2 rounded-xl text-white font-bold text-base'
                     >
                         <Image
@@ -92,7 +91,7 @@ const Landing = () => {
                             alt=''
                         /> 
                         Connect Metamask
-                    </button>
+                    </button> */}
                 </div>
             </div>
         </div>
